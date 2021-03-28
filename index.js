89677bc6a8afc4e3d2b64e99ade8cb3a9c3114de
rangasubhecollection <rangasubhecollection@gmail.com>
2021-03-28 12:56:37 +0530

    sss

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var rect = {
 	perimeter: (x, y) => (2*(x+y)),
 	area: (x, y) => (x*y)
 };
-
-
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
@@ -11,7 +9,7 @@ function solveRect(l,b) {
         console.log("Rectangle dimensions should be greater than zero:  l = "
                + l + ",  and b = " + b);
     }
-    
+
     else {
 	    console.log("The area of the rectangle is " + rect.area(l,b));
 	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
